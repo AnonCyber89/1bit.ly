6f93f83c000bd2f0398b448846f8980c409a2e4d
AnonCyber89 <31952805+AnonCyber89@users.noreply.github.com>
2021-03-15 22:22:12 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,30 @@
+<?
+
+# User-Agent
+
+$uag = "xxxxxxxx";
+
+
+# Cookie
+
+$cook = 'xxxxxxxxxx';
+
+
+
+# If You Have Akses To Withdraw👇
+
+
+
+# On/Off
+
+$wd = "Off";
+
+
+
+# Amount Bits To Wd (exemple. 10000)
+
+$value = "10000";
+
+
+
+?>
